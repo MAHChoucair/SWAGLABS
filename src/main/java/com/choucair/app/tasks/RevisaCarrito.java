000f33f94e222a8f154e3626c738d4c363be24aa
MelisaAH10 <melisaa562@gmail.com>
2024-09-15 16:57:59 -0500

    update 15-09 16:57

--- src/main/java/com/choucair/app/tasks/RevisaCarrito.java
+++ src/main/java/com/choucair/app/tasks/RevisaCarrito.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.PerformsTouchActions;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
@@ -77,21 +78,13 @@ public class RevisaCarrito implements Task {
                     .isPresent();
 
             if (!isCheckoutVisible) {
-                // Realiza un pequeño scroll hacia abajo
-                TouchAction action = new TouchAction((PerformsTouchActions) BrowseTheWeb.as(actor).getDriver());
-
-                // Ajusta los valores para un desplazamiento corto
-                Dimension dimension = BrowseTheWeb.as(actor).getDriver().manage().window().getSize();
-                int startX = dimension.width / 2;
-                int startY = (int) (dimension.height * 0.8);  // Empieza desde el 80% de la altura
-                int endY = (int) (dimension.height * 0.2);    // Desplázate hasta el 20% de la altura
-
-                // Ejecuta el scroll
-                action.press(PointOption.point(startX, startY))
-                        .waitAction(WaitOptions.waitOptions(Duration.ofMillis(500)))
-                        .moveTo(PointOption.point(startX, endY))
-                        .release()
-                        .perform();
+                // Utiliza UiScrollable para desplazarte hacia abajo en el contenedor desplazable
+                WebElement scrollable = BrowseTheWeb.as(actor).getDriver().findElement(
+                        MobileBy.AndroidUIAutomator(
+                                "new UiScrollable(new UiSelector().scrollable(true))" +
+                                        ".scrollForward();"
+                        )
+                );
             }
         }
 
